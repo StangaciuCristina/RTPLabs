7728dc9f28a9ac8429633263542c96d84f0152ad
cristina.stagaciu <certejan@dsplabs.cs.upt.ro>
2024-02-21 14:43:05 +0200

    Aplication 25 added
    Signed-off-by: cristina.stagaciu <certejan@dsplabs.cs.upt.ro>

--- RTPLaboratories/Core/Src/main.c
+++ RTPLaboratories/Core/Src/main.c
@@ -17,7 +17,7 @@
 */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include <application24.h>
+#include <application25.h>
 #include "main.h"
 #include "cmsis_os.h"
 #include "usb_host.h"
@@ -132,7 +132,7 @@ int main(void)
 
   /* USER CODE BEGIN RTOS_THREADS */
 	/* add threads, ... */
-	application24();
+	application25();
   /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
